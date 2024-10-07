7468ba4bc514bee2553ae4790cb9bbb77c4aa93f
leonardovictormuller <84246772+LeonardoVictorMuller@users.noreply.github.com>
2024-10-07 21:00:55 +0000

    colocando um abertura, e preparando a parte para colocar a sala

--- config.js
+++ config.js
@@ -2,5 +2,12 @@ export default {
     type: Phaser.AUTO,
     width: 800,
     height: 450,
-    parent: 'game-conteiner'
+    parent: 'game-conteiner',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'game-container',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 450
+    }
 }
