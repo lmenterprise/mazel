777539fa5ec449bd2409f4cc348eff8d2fd23947
leonardovictormuller <84246772+LeonardoVictorMuller@users.noreply.github.com>
2024-10-14 20:32:31 +0000

    criando o mapa.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import config from './config.js'
 import abertura from './abertura.js'
 import sala from './sala.js'
+import mapa from './mapa.js'
 
 class Game extends Phaser.Game {
   constructor () {
     super(config)
     this.scene.add('abertura', abertura)
     this.scene.add('sala', sala)
-    this.scene.start('abertura')
+    this.scene.add('mapa', mapa)
+    this.scene.star('mapa')
   }
 }
 
---
+++ mapa.js
@@ -0,0 +1,55 @@
+export default class mapa extends Phaser.Scene {
+  constructor () {
+    super('mapa')
+  }
+
+  preload () {
+
+    this.load.tilemapTiledJSON('mapa', 'assests/mapa/mapa.json')
+    this.load.image('estrutura', 'assests/mapa/estrutura.json')
+    this.load.image('grama', 'assests/mapa/Brick_Texture.json')
+    
+
+    this.load.spritesheet('personagem', 'assets/menino_sem_picareta.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
+  }
+
+  create () {
+    this.tilemap = this.make.tilemap({ key: 'map' })
+    this.tilesetEstrutura = this.tilemapMapa.addTilersetImage('estrutura')
+    this.tilesGrama = this.tilemapMapa.addTilersetImage('grama')
+
+
+    this.layerChao = this.tilemapMapa.creatStaticLayer('chao', [this.tilesetgrama], 0, 0)
+    this.layerMoveis = this.tilemapMapa.creatStaticLayer('moveis', [this.tilesetestrutura], 0, 0)
+
+
+
+    this.anims.create({
+      key: 'parado',
+      frames: [{ key: 'personagem', frame: 15 }],
+      frameRate: 1
+    })
+
+    this.anims.create({
+      key: 'andar-direita',
+      frames: this.anims.generateFrameNumbers('personagem', { start: 88, end: 96 }),
+      frameRate: 10,
+      repeat: -1
+    })
+
+    this.personagem = this.physics.add.sprite(50, 225, 'personagem', 15)
+      .setInteractive()
+      .on('pointerdown', () => {
+        if (this.personagem.body.velocity.x === 0) {
+          this.personagem.setVelocityX(100)
+          this.personagem.anims.play('andar-direita')
+        } else {
+          this.personagem.setVelocityX(0)
+          this.personagem.anims.play('parado')
+        }
+      })
+  }
+}
